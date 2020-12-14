2ca4d96fb1e1efb0db8f1c033651e21d0e4e5994
joris-verguldezoone <joris.verguldezoone@laplateforme.io>
2020-12-14 16:23:34 +0100

    LPDO MANGE MES ********

--- lpdo.php
+++ lpdo.php
@@ -13,32 +13,32 @@ class lpdo{
 
     public function __construct($host, $username, $password, $db)
     {
-        $this->bdd = new mysqli($host, $username, $password, $db);
-        echo 'Connexion avec succès...';
+        $this->bdd = mysqli_connect($host, $username, $password, $db);
+        echo 'Connexion avec succès...<br/ >';
     }
     public function __destruct()
     {
-        print "HARDJOJO has just Destroyed " . __CLASS__ . "\n"; // mmmmmmmmmmmmmmmmmmmmmmmmhhhhhh very good
+        print "<br/ >HARDJOJO has just Destroyed " . __CLASS__ . "\n"; // mmmmmmmmmmmmmmmmmmmmmmmmhhhhhh very good
     }
     public function close()
     {
         $this->bdd->close();
-        echo('la connexion a ete fermée');
+        echo('<br/ >la connexion a ete fermée<br/ >');
 
     }
     /*execute($query)
 Exécute la requête $query et retourne un tableau contenant la réponse du
 serveur SQL.*/
     public function execute($query){
-        $this->mysqli_query = mysqli_query($this->bdd,$query);
-        $mysqli_query = $this->mysqli_query;
-        if($mysqli_query)
-        echo '<br />';
-        while($result = mysqli_fetch_assoc($mysqli_query)){
-            echo "<table><tr><td  style='border: black 1px solid;'>".$result['id']."</td><td style='border: black 1px solid;'>".$result['login']. "</td><td style='border: black 1px solid;'>" .
-            $result['password']. "</td><td style='border: black 1px solid;'>" .$result['email']. "</td><td style='border: black 1px solid;'>" .$result['firstname']. "</td><td style='border: black 1px solid;'>" .
-            $result['lastname']. "</td></tr></table>";
-        }
+        $bdd = $this->bdd;
+        $this ->query = $query;
+        
+        $mysqli_query = mysqli_query($bdd,$query);
+        $fetch = mysqli_fetch_assoc($mysqli_query);
+        print_r($fetch);
+        echo '<br/ >reponse execute<br/ >';
+
+        return $fetch;
      
     
     }
@@ -46,9 +46,9 @@ serveur SQL.*/
 Retourne la dernière requête SQL ayant été exécutée, false si aucune
 requête n’a été exécutée.*/
     public function getLastQuery(){
-       $mysqli_query = $this->mysqli_query;
-        if($mysqli_query) // true
-        var_dump($mysqli_query);
+       $query = $this->query;
+        if($query) // true
+        var_dump($query);
         else //false
         echo'false';                  
         
@@ -58,50 +58,62 @@ requête n’a été exécutée.*/
 Retourne le résultat de la dernière requête SQL exécutée, false si aucune
 requête n’a été exécutée.*/
     public function getLastResult(){
-
-           $lastResult = $this->result;
-           var_dump($lastResult);
-           $mysqli_query = $this->mysqli_query;
-           var_dump($mysqli_query);
-           if($lastResult)
-           while($lastResult = mysqli_fetch_assoc($mysqli_query)){
-              echo $lastResult['login'],$lastResult['password'],$lastResult['email'],$lastResult['firstname'],$lastResult['lastname'];
-           }
-        //    else
-        //    echo 'false';
-
-
-
+        $bdd = $this->bdd;
+        $getLastResult = $this->query;
+        $lastResult = $this->execute($getLastResult);
+        return $lastResult;
     }
     /*getTables()
 Retourne un tableau contenant la liste des tables présentes dans la base
 de données.*/
     public function getTables(){
+        
         $bdd = $this->bdd;
-        $sql = "SHOW TABLES FROM classes";
-        $send = mysqli_query($bdd,$sql);
-        $fetch = mysqli_fetch_all($send);
-
-        var_dump($fetch);
+        $appelfonction = $this->execute("SHOW TABLES"); 
+    if($appelfonction){
+        // foreach pour que ça retourne toutes les column;
+    
+        print_r($appelfonction);
+        return $appelfonction;
+    }
+    else
+    echo 'ahah niqué';
     }
 
+   /* - getFields($table)
+Retourne un tableau contenant la liste des champs présents dans la table
+passée en paramètre, false en cas d’erreur.*/
+    public function getFields($table){
+        $tableInfo = $this->execute($table);
+
+        if($tableInfo) {
+            return($tableInfo);
+        }           
+        
+    }
 }
 //--------------------------------------------------fin de la classe-------------------------------------------------------------
 try
 {
     $joris = new lpdo('localhost', 'root',   '', 'classes');
-    var_dump($joris);
+    $joris->execute("SELECT * FROM utilisateurs");
+
+$joris->getLastQuery();
+echo '<br/ >getLastQuery <br/ >';
+$joris->getLastResult(); //marche pas
+echo '<br/ >getLastResult <br/ >';
+$joris->getTables();
+echo '<br/ >getTable <br/ >';
+$joris->getFields("SHOW COLUMNS FROM utilisateurs");
+echo 'getFields deuhman';
+$joris->close();
 }
 catch (Exception $e) // errorLog
 {
     print ('Erreur : ' . $e -> getMessage() . '<br />');
 }
-// $joris->close();
 
-$joris->execute("SELECT * FROM utilisateurs");
 
-$joris->getLastQuery();
-$joris->getLastResult();
-$joris->getTables();
+
 ?>
 
